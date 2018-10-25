8fde22a83cafa37a5c5c0ef5714ad0ec027545da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-25 17:24:31 +0000

    Deploy Z-Editor/z-editor.github.io to github.com/Z-Editor/z-editor.github.io.git:master

---
+++ precache-manifest.03636637ac77329ea14e3dbf6be6d3d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e06c8bbf204270dd0d41f26130f004b6",
+    "url": "/static/media/inverse.e06c8bbf.svg"
+  },
+  {
+    "revision": "b83dd540104e485be0c6d8465ecdb53d",
+    "url": "/static/media/half.b83dd540.svg"
+  },
+  {
+    "revision": "96533a9799513cd06deb67f949b34dae",
+    "url": "/static/media/full.96533a97.svg"
+  },
+  {
+    "revision": "c521700d3818042f316019bfdc0f1691",
+    "url": "/static/media/bar.c521700d.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a56bec83cf7da95cefd",
+    "url": "/static/js/main.1a56bec8.chunk.js"
+  },
+  {
+    "revision": "f34556208ec15176b98e",
+    "url": "/static/js/1.f3455620.chunk.js"
+  },
+  {
+    "revision": "1a56bec83cf7da95cefd",
+    "url": "/static/css/main.cb73e2db.chunk.css"
+  },
+  {
+    "revision": "1e470320a630614da8fa9fada0f1d213",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db273fa1f1699628881e715d24f644fe.js"
+  "/precache-manifest.03636637ac77329ea14e3dbf6be6d3d9.js"
 );
 
 workbox.clientsClaim();
