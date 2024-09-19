d3842a13dc55745bd5a0e38944dd4fd08b847d01
Nadun Indunil <nadun1indunil@gmail.com>
2024-09-19 17:19:46 +1000

    Add analytics scripts
    Add analytics scripts to log app views.

---
+++ precache-manifest.80b05943075bd4fad166f8119104ae09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3008da4f0ffa79e8264392c29cb5ef2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e79388f74fcd42f2ef",
+    "url": "/static/css/main.64606131.chunk.css"
+  },
+  {
+    "revision": "e24cb3ccc28383ce2c62",
+    "url": "/static/js/2.f8a42da0.chunk.js"
+  },
+  {
+    "revision": "b49d7265f1a7beaf69e0ca94d5a4662d",
+    "url": "/static/js/2.f8a42da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e79388f74fcd42f2ef",
+    "url": "/static/js/main.726470f5.chunk.js"
+  },
+  {
+    "revision": "5fbadfe9cce23d4ce5d3c4381be0212e",
+    "url": "/static/js/main.726470f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e999ccf81702143a718",
+    "url": "/static/js/runtime-main.6fd88b8f.js"
+  },
+  {
+    "revision": "c521700d3818042f316019bfdc0f1691",
+    "url": "/static/media/bar.c521700d.svg"
+  },
+  {
+    "revision": "96533a9799513cd06deb67f949b34dae",
+    "url": "/static/media/full.96533a97.svg"
+  },
+  {
+    "revision": "b83dd540104e485be0c6d8465ecdb53d",
+    "url": "/static/media/half.b83dd540.svg"
+  },
+  {
+    "revision": "e06c8bbf204270dd0d41f26130f004b6",
+    "url": "/static/media/inverse.e06c8bbf.svg"
+  }
+]);
